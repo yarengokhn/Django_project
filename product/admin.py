ae2c0d56764c674a4acfd74d1fad6faaf7c37a48
yaren <yarengokhn@gmail.com>
2024-10-27 15:45:03 +0300

    Added product and prouct admin.

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from product.models import Category, Product
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    #fields = ['title','status']
+    list_display =['title','status']
+    list_filter = ['status']
+    prepopulated_fields={"slug":("title","keywords")}
+
+admin.site.register(Category,CategoryAdmin) #category yi görebiliyoruzn admin tarafinda
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    #fields = ['title','status']
+    list_display =['title','price','amount','status']
+    list_filter = ['status','category']
+    #readonly_fields = ['price']
+    prepopulated_fields={"slug":("category","title")}
+
+admin.site.register(Product,ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -16,10 +17,41 @@ class Category(models.Model):
     slug = models.SlugField(null=False,unique=True) #muhakkak doldurulmasi gerektigi icin null False yaptik.
     parent = models.ForeignKey('self',blank=True,null=True,related_name='children',on_delete=models.CASCADE)
 
-    create_at =models.DateTimeField(auto_now=True)
+    create_at =models.DateTimeField(auto_now_add=True)
     update_at =models.DateTimeField(auto_now=True)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    STATUS = (
+    ('True','Evet'),
+    ('False','Hayir'),)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE) #many to one relation #on delete o satir silinirse baglanti olanlarin tümünü sil anlamina geliyor
+
+    title =models.CharField(max_length=150)
+    keywords =models.CharField(blank=True,max_length=255)#bos birakilabilir.
+    description =models.CharField(blank=True,max_length=255)
+    image = models.ImageField(blank=True,upload_to='images/')
+
+    price = models.FloatField()
+    amount = models.IntegerField(default=0)
+
+    details = models.TextField()
+    status =models.CharField(max_length=10,choices=STATUS,default='False')
+    
+    create_at =models.DateTimeField(auto_now_add=True)
+    update_at =models.DateTimeField(auto_now=True)
+    slug = models.SlugField(null=False,unique=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+    
 
 
