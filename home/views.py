fd779eaad6f296ef02746974b677a2d11bdd4617
yaren <yarengokhn@gmail.com>
2024-11-07 18:24:05 +0300

    Added form submission handling and data saving

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from home.forms import ContactForm
-from home.models import Settings
+from home.models import ContactFormMessage, Settings
 
 # Create your views here.
 
@@ -10,8 +11,22 @@ def index(request):
     return render(request,'index.html',context)
 
 
-
 def contact(request):
+    if request.method =='POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name =form.cleaned_data['name']
+            data.email =form.cleaned_data['email']
+            data.message =form.cleaned_data['message']
+            data.subject =form.cleaned_data['subject']
+            data.ip = request.META['REMOTE_ADDR']
+            data.save()
+            messages.success(request, 'Mesajınız Başarıyla İletilmiştir')
+            return HttpResponseRedirect('/contact')
+        else:
+            messages.error(request,'Mesajınız Sisteme Kaydedilmemiştir')
+            return HttpResponseRedirect('/contact')
     form = ContactForm()
     context = {"sayfa": "Contact -Iletisim",
                'form': form}
